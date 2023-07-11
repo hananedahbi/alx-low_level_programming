37a4902c83a7e7c73e890136fc01f9496a1d6c25
Hananedahbi123 <133127390+hananedahbi@users.noreply.github.com>
2023-07-11 09:25:08 +0200

    Update main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,4 +1,6 @@
 #ifndef MAIN_H
+#define MAIN_H
+
 
 int _putchar(char c);
 int _islower(int c);
@@ -21,4 +23,5 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* MAIN_H */
+
+#endif
