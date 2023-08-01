5f2bc4f160970aacfb8a0e2cb39774719391fa20
Hananedahbi123 <133127390+hananedahbi@users.noreply.github.com>
2023-08-01 16:29:53 +0200

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,40 +1,41 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - Deletes the node at index index of a listint_t linked list.
- * @head: Pointer to a pointer to the first node.
- * @index: Index of the node to be deleted.
+ * delete_nodeint_at_index - Deletes the node at index
+ * @head: address of pointer to first node
+ * @index: Index of the node to be deleted
  *
- * Return: 1 if it succeeded, -1 if it failed.
+ * Return: 1 if it succeeded, -1 if it failed
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+    listint_t *node, *prev_node;
+    unsigned int i = 0;
+
     if (!head || !(*head))
         return (-1);
 
     if (index == 0)
     {
-        listint_t *temp = *head;
+        node = *head;
         *head = (*head)->next;
-        free(temp);
+        free(node);
         return (1);
     }
 
-    listint_t *current = *head;
-    unsigned int i = 0;
-
-    while (current && i < index - 1)
+    node = *head;
+    while (node)
     {
-        current = current->next;
+        if (i == index)
+        {
+            prev_node->next = node->next;
+            free(node);
+            return (1);
+        }
         i++;
+        prev_node = node;
+        node = node->next;
     }
 
-    if (!current || !(current->next))
-        return (-1);
-
-    listint_t *temp = current->next;
-    current->next = current->next->next;
-    free(temp);
-
-    return (1);
-}
+    return (-1);
+}     
