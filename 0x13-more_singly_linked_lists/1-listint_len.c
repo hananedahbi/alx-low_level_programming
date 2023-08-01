e877f2d4f9790f7b3b8e0faf935deb65e88a73fe
Hananedahbi123 <133127390+hananedahbi@users.noreply.github.com>
2023-08-02 01:39:46 +0200

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,21 +1,21 @@
  #include "lists.h"
 
 /**
- * count_elements - Counts the number of elements in a linked listint_t list.
+ * get_list_length - Counts the number of elements in a linked listint_t list.
  * @h: Pointer to the list's first node.
  *
  * Return: The number of nodes in the list.
  **/
-size_t count_elements(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
     const listint_t *node = h;
-    size_t count = 0;
+    size_t cont = 0;
 
     while (node)
     {
-        count++;
+        cont++;
         node = node->next;
     }
 
-    return (count);
-}   
+    return (cont);
+}
