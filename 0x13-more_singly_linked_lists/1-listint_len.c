7f353c0c1869778dbd716d964b0bc1e7324f41ae
Hananedahbi123 <133127390+hananedahbi@users.noreply.github.com>
2023-08-02 01:22:50 +0200

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,21 +1,20 @@
 #include "lists.h"
 
 /**
- * custom_listint_len - Returns the number of elements in a linked list.
- * @head: Pointer to the list's first node.
- *
- * Return: The number of nodes in the list.
+ * listint_len - returns the number of elements in a linked listint_t list.
+ * @h: pointer to the list.
+ * Return: number of nodes.
  **/
-size_t custom_listint_len(const listint_t *head)
+size_t listint_len(const listint_t *h)
 {
-    const listint_t *current = head;
-    size_t element_count = 0;
+	const listint_t *node = h;
+	size_t cont = 0;
 
-    while (current)
-    {
-        element_count++;
-        current = current->next;
-    }
+	while (node)
+	{
+		cont++;
+		node = node->next;
+	}
 
-    return (element_count);
+	return (cont);
 }
