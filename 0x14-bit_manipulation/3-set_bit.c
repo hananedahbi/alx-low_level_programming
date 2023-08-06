fe70be42effcf822356746436615edf63d1753d1
Hananedahbi123 <133127390+hananedahbi@users.noreply.github.com>
2023-08-06 15:05:29 +0200

    Create 3-set_bit.c

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * set_bit - Set bit
+ * @n: Decimal number
+ * @index: Position index
+ * Return: 1 if successful, -1 if error
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int count;
+
+	if (index > 64)
+		return (-1);
+
+	for ( count= 1; index > 0; index--, count *= 2)
+		;
+	*n += count;
+	return (1);
+}
