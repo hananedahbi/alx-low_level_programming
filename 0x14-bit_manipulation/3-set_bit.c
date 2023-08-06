87b47f11a2b8c2a107a8fd6b2caffb6eb97f0a72
Hananedahbi123 <133127390+hananedahbi@users.noreply.github.com>
2023-08-06 18:14:45 +0200

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -6,15 +6,15 @@
  * @index: Position index
  * Return: 1 if successful, -1 if error
  */
-int set_bit(unsigned long int *n, unsigned int index)
-{
+ {
 	unsigned long int count;
 
 	if (index > 64)
 		return (-1);
 
-	for ( count= 1; index > 0; index--, count *= 2)
+	for (count = 1; index > 0; index--, count *= 2)
 		;
 	*n += count;
+
 	return (1);
 }
