e2378001fbee4a3fabfb5bbd042bfdda71fc34b1
Hananedahbi123 <133127390+hananedahbi@users.noreply.github.com>
2023-08-07 22:06:51 +0200

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,36 +6,33 @@
  * @num_letters: Number of letters
  * Return: Actual number
  */
-ssize_t read_textfile(const char *filename, size_t num_letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int descriptor, read_count, written_count;
-    char *buffer;
+	int fd;
+	ssize_t read_count, write_count;
+	char *buffer;
 
-    if (!filename)
-        return (0);
-
-    descriptor = open(filename, O_RDONLY);
-    if (descriptor == -1)
-        return (0);
-
-    buffer = malloc(num_letters * sizeof(char));
-    if (!buffer)
-    {
-        close(descriptor);
-        return (0);
-    }
-
-    read_count = read(descriptor, buffer, num_letters);
-    written_count = write(STDOUT_FILENO, buffer, read_count);
-
-    if (written_count < 0)
-    {
-        free(buffer);
-        close(file_descriptor);
-        return (0);
-    }
-
-    free(buffer);
-    close(file_descriptor);
-    return (read_count);
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	read_count = read(fd, buffer, letters);
+	close(fd);
+	if (read_count == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	write_count = write(STDOUT_FILENO, buffer, read_count);
+	free(buffer);
+	if (read_count != write_count)
+		return (0);
+	return (write_count);
 }
