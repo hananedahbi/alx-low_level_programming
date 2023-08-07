2e9140ff0976f7881d240a0f573b7997658a704f
Hananedahbi123 <133127390+hananedahbi@users.noreply.github.com>
2023-08-07 22:17:17 +0200

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,38 +1,36 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * read_textfile - Read file
  * @filename: File name
- * @num_letters: Number of letters
+ * @letters: Number of letters
  * Return: Actual number
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t read_count, write_count;
+	ssize_t file_desc, read_count, write_count;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
-	buffer = malloc(sizeof(char) * letters);
+
+	buffer = malloc(sizeof(char) * num_letters);
 	if (buffer == NULL)
-	{
-		close(fd);
 		return (0);
-	}
-	read_count = read(fd, buffer, letters);
-	close(fd);
-	if (read_count == -1)
+
+	file_desc = open(filename, O_RDONLY);
+	read_count = read(file_desc, buffer, num_letters);
+	write_count = write(STDOUT_FILENO, buffer, read_count);
+
+	if (file_desc == -1 || read_count == -1 || write_count == -1 || write_count != read_count)
 	{
 		free(buffer);
 		return (0);
 	}
-	write_count = write(STDOUT_FILENO, buffer, read_count);
+	
 	free(buffer);
-	if (read_count != write_count)
-		return (0);
+	close(file_desc);
+
 	return (write_count);
 }
