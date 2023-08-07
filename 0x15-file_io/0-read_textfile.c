38f5d12e7465feec861e1a798abf6bdb5d2bfdb4
Hananedahbi123 <133127390+hananedahbi@users.noreply.github.com>
2023-08-07 23:05:46 +0200

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,48 +1,52 @@
 #include <unistd.h>
+#include <fcntl.h>
 #include <stdlib.h>
 #include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints it to POSIX stdout.
- * @filename: Pointer to the name of the file.
- * @letters: The number of letters the function should read and print.
+ * read_textfile - Read and print a text file to POSIX stdout.
+ * @filename: Pointer to file name.
+ * @num_letters: Number of letters to read and print.
  *
- * Return: If the function fails or filename is NULL - 0.
- *         O/w - the actual number of letters it could read and print.
+ * Return: If failed or filename is NULL - 0.
+ *         O/w - actual bytes the function can read and print.
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t num_letters)
 {
 	ssize_t file_desc, read_count, write_count;
-	char *buffer;
+	char *buf;
 
 	if (filename == NULL)
 		return (0);
 
-	buffer = malloc(letters * sizeof(char));
-	if (buffer == NULL)
+	buf = malloc(sizeof(char) * num_letters);
+	if (buf == NULL)
 		return (0);
 
 	file_desc = open(filename, O_RDONLY);
 	if (file_desc == -1)
 	{
-		free(buffer);
+		free(buf);
 		return (0);
 	}
 
-	read_count = read(file_desc, buffer, letters);
-	close(file_desc);
-
+	read_count = read(file_desc, buf, num_letters);
 	if (read_count == -1)
 	{
-		free(buffer);
+		free(buf);
+		close(file_desc);
 		return (0);
 	}
 
-	write_count = write(STDOUT_FILENO, buffer, read_count);
-	free(buffer);
-
-	if (write_count == -1 || (size_t)write_count < letters)
+	write_count = write(STDOUT_FILENO, buf, read_count);
+	if (write_count == -1 || write_count != read_count)
+	{
+		free(buf);
+		close(file_desc);
 		return (0);
+	}
 
-	return (read_count);
+	free(buf);
+	close(file_desc);
+	return (write_count);
 }
