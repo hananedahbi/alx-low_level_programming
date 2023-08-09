e8671d3e81ac0c3e786c24d456e8328f2e506506
Hananedahbi123 <133127390+hananedahbi@users.noreply.github.com>
2023-08-09 20:07:58 +0200

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,39 +2,38 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include "main.h"
 
 #define BUFFER_SIZE 1024
 
 /**
- * main - Entry point for the cp program.
- * @ac: Argument count.
- * @av: Argument vector.
+ * main - Copies the content of one file to another.
+ * @argc: Argument count.
+ * @argv: Argument vector.
  *
  * Return: 0 on success, or appropriate error code.
  */
-int main(int ac, char **av)
+int main(int argc, char **argv)
 {
 	int fd_from, fd_to, read_bytes, write_bytes;
 	char buffer[BUFFER_SIZE];
 
-	if (ac != 3)
+	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", av[0]);
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
 		exit(97);
 	}
 
-	fd_from = open(av[1], O_RDONLY);
+	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
-	fd_to = open(av[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	fd_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		close(fd_from);
 		exit(99);
 	}
@@ -44,7 +43,7 @@ int main(int ac, char **av)
 		write_bytes = write(fd_to, buffer, read_bytes);
 		if (write_bytes == -1 || write_bytes != read_bytes)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(fd_from);
 			close(fd_to);
 			exit(99);
@@ -53,7 +52,7 @@ int main(int ac, char **av)
 
 	if (read_bytes == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		close(fd_from);
 		close(fd_to);
 		exit(98);
