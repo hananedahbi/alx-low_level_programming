b4fe888660f1eacc17eb943447f38a25c6e6c5bc
Hananedahbi123 <133127390+hananedahbi@users.noreply.github.com>
2023-10-28 00:23:18 +0200

    Create 2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int i;
+
+	if (index > 64)
+		return (-1);
+
+	i = n >> index;
+
+	return (i & 1);
+}
