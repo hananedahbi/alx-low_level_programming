7e797479c09590ac26bba513ee22f94268d9b8d5
Hananedahbi123 <133127390+hananedahbi@users.noreply.github.com>
2023-10-28 00:31:52 +0200

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,11 +1,25 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-int _putchar(char c);
-unsigned int binary_to_uint(const char *b);
-void print_binary(unsigned long int n);
-int get_bit(unsigned long int n, unsigned int index);
-int set_bit(unsigned long int *n, unsigned int index);
-int clear_bit(unsigned long int *n, unsigned int index);
-unsigned int flip_bits(unsigned long int n, unsigned long int m);
-int get_endianness(void);
-#endif
+#include "main.h"
+#include <stdio.h>
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int total, power;
+	int j;
+
+	if (b == NULL)
+		return (0);
+
+	for (j = 0; b[len]; j++)
+	{
+		if (b[j] != '0' && b[j] != '1')
+			return (0);
+	}
+
+	for (power = 1, total = 0, j--; j >= 0; j--, power *= 2)
+	{
+		if (b[j] == '1')
+			total += power;
+	}
+
+	return (total);
+}
