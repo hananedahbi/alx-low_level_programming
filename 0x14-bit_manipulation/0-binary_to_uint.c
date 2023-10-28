1b0bbad79df8d104ea3fe426740205289ff9c347
Hananedahbi123 <133127390+hananedahbi@users.noreply.github.com>
2023-10-28 09:58:44 +0200

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,20 +4,20 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int total, power;
-	int j;
+	int n;
 
 	if (b == NULL)
 		return (0);
 
-	for (j = 0; b[len]; j++)
+	for (n = 0; b[n]; n++)
 	{
-		if (b[j] != '0' && b[j] != '1')
+		if (b[n] != '0' && b[n] != '1')
 			return (0);
 	}
 
-	for (power = 1, total = 0, j--; j >= 0; j--, power *= 2)
+	for (power = 1, total = 0, n--; n >= 0; n--, power *= 2)
 	{
-		if (b[j] == '1')
+		if (b[n] == '1')
 			total += power;
 	}
 
