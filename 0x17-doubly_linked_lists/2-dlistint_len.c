3cf7d15f7ce0a3c27ce148ef301a121048dd120d
Hananedahbi123 <133127390+hananedahbi@users.noreply.github.com>
2023-12-07 16:33:13 +0100

    Update 2-dlistint_len.c

--- 0x17-doubly_linked_lists/2-dlistint_len.c
+++ 0x17-doubly_linked_lists/2-dlistint_len.c
@@ -1,22 +1,40 @@
 #include "lists.h"
 
 /**
- * dlistint_len - function that returns the number of elements in a,
- * linked dlistint_t list.
- * @h: poiner to the head of the list
+ * add_dnodeint - function that adds new node at begininning of a dbly,
+ * linked list.
+ * @head: pointer to pointer to head of list.
+ * @n: data to be added.
  *
- * Return: number of elements
+ * Return: the address of the new element, or NULL if it failed.
  */
 
-size_t dlistint_len(const dlistint_t *h)
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	size_t count = 0;
+	dlistint_t *new_node;
 
-	/* while we have a pointer to the list */
-	while (h != NULL)
-	{
-		count++;
-		h = h->next;
-	}
-	return (count);
+	/* allocate memory for new node */
+	new_node = malloc(sizeof(dlistint_t));
+
+	/* if it fails return NULL */
+	if (new_node == NULL)
+		return (NULL);
+
+	/* assign data to new node */
+	new_node->n = n;
+
+	/* point next of newNode to the first node of the doubly linked list */
+	new_node->next = *head;
+
+	/* point previous of new node to NULL */
+	new_node->prev = NULL;
+
+	/* point prev of 1st node (now 1st node is the second node) to new node */
+	if ((*head) != NULL)
+		(*head)->prev = new_node;
+
+	/* head points to newNode */
+	(*head) = new_node;
+
+	return (new_node);
 }
